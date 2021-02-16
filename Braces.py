c4b7f7691f4d579441cf716bfa1be1fde8596518
Armaan Gupta <guptaarmaan774@gmail.com>
2021-02-16 12:35:32 +0530

    Added The Parser Class

--- Braces.py
+++ Braces.py
@@ -1,13 +1,20 @@
+# Imports #
 import os
 import sys
+
 '''------------------------CONSTANTS--------------------------------------'''
 KEYWORDS = ["echo", "while", "for", "with", "iter"]
 ALPHABETS = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
 NUMBERS = "1234567890."
 IGNORE = [" ", "\t", "\n"]
 OPERATORS = ["+", "-", "/", "*", "%"]
+L_BRACKET = '('
+R_BRACKET = ')'
+SEMI_COLON = ";"
+
 '''-----------------------------------------------------------------------'''
-'''------------------------GENERAL FUNCTIONS------------------------------'''
+'''------------------------|GENERAL FUNCTIONS|------------------------------'''
+'''-----------------------------------------------------------------------'''
 def parse_numbers(brace):
     str_num = ""
     decimal_count = 0
@@ -31,15 +38,23 @@ def parse_numbers(brace):
 
     return [num, isFloat]
 
-'''---------------------------GENERAL CLASSES--------------------------------'''
 
+'''---------------------------GENERAL CLASSES--------------------------------'''
 class Token:
     def __init__(self, type_, value):
         self.type_ = type_
         self.value = value
 
     def __repr__(self):
-        return f'[Type: {self.type_} | Value: {self.value}]'
+        return f'[{self.type_} | {self.value}]'
+
+class OperatorNode:
+    def __init__(self, type_, value):
+        self.type_ = type_
+        self.value = value
+    
+    def __repr__(self):
+        return f'[{self.type_} | {self.value}]'
 
 class Error:
     def __init__(self, type_, message):
@@ -56,8 +71,29 @@ class TypoError(Error):
     def activate(self):
         self.activate()
 
-'''--------------------------MAIN BRACE CLASS--------------------------------'''
+class Node:
+    def __init__(self, type_, value):
+        self.type_ = type_
+        self.value = value
+    def __repr__(self):
+        return self.value
 
+class BinaryOp:
+    def __init__(self, op, left, right):
+        self.op = op
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f'({self.left} {self.op} {self.right})'
+
+class NumberNode:
+    def __init__(self, value):
+        self.value = value
+    def __repr__(self):
+        return f'{self.value}'
+
+'''--------------------------MAIN BRACE CLASS--------------------------------'''
 class Braces:
     def __init__(self, code):
         self.code = code
@@ -67,6 +103,7 @@ class Braces:
         self.tokens = []
         self.onString = False
         self.curr_string = ''
+        self.Nodes = []
 
     def advance(self):
         self.current_index += 1
@@ -111,6 +148,72 @@ class Braces:
 
             if self.current_char in OPERATORS:
                 self.text += self.current_char
-                self.tokens.append(Token("Operator", self.current_char))
+                self.tokens.append(OperatorNode("Operator", self.current_char))
+
+            if self.current_char == L_BRACKET:
+                self.text += self.current_char
+                self.tokens.append(Token("L_BRACKET", self.current_char))
+
+            if self.current_char == R_BRACKET:
+                self.text += self.current_char
+                self.tokens.append(Token("R_BRACKET", self.current_char))
+            
+            if self.current_char == SEMI_COLON:
+                self.text += self.current_char
+                self.tokens.append(Token("SEMI_COLON", self.current_char))
 
             self.advance()
+
+    def echo(self, string):
+        print(string)
+
+
+class Parser():
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.token_idx = 0
+        self.current_token = self.tokens[self.token_idx]
+        self.nodes = []
+
+    def advance(self):
+        self.token_idx += 1
+        if self.token_idx < len(self.tokens):
+            self.current_token = self.tokens[self.token_idx]
+        return self.token_idx
+
+    def factors(self):
+        if self.current_token.type_ in ("Integer", "Float"):
+            number = NumberNode(self.current_token)
+            self.advance()
+            return number
+    
+    def binary_op(self, func, ops):
+        left = func()
+        while self.current_token.value in ops:
+            op = self.current_token
+            self.advance()
+            right = func()
+            left = BinaryOp(op, left, right)
+
+        return left
+
+    def parse(self):
+        return self.expr()
+
+    def term(self):
+        return self.binary_op(self.factors, ("*", "/"))
+
+    def expr(self):
+        return self.binary_op(self.term, ("+", "-"))
+
+if __name__ == "__main__":
+    while True:
+        command = input("Brace $> ")
+        if command == "exit":
+            exit()
+        brace = Braces(command)
+        brace.gen_tokens()
+        parser = Parser(brace.tokens)
+        ast = parser.parse()
+        print(ast)
+        print(brace.tokens)
--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 from Braces import Braces
+from Braces import Parser
 
 with open("test.brace", "r") as f:
     code = f.read()
     brace = Braces(code)
     brace.gen_tokens()
-    print(brace.tokens)
-    
+    parser = Parser(brace.tokens)
+    ast = parser.parse()
+    print(ast)
